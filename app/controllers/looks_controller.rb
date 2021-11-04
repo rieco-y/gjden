d56f50823e2c4bad5736ab3cfac365a7db1b8168
rieco-y <90105763+rieco-y@users.noreply.github.com>
2021-11-04 22:46:33 +0900

    looksコントローラー修正

--- app/controllers/looks_controller.rb
+++ app/controllers/looks_controller.rb
@@ -1,15 +1,20 @@
 class LooksController < ApplicationController
+  before_action :reservation_params
+
 
   def create
     @look = current_user.looks.create(reservation_id: params[:reservation_id])
-    redirect_back(fallback_location: root_path)
   end
 
   def destroy
     @look = Look.find_by(reservation_id: params[:reservation_id], user_id: current_user.id)
     @look.destroy
-    redirect_back(fallback_location: root_path)
   end
 
+  private
+
+  def reservation_params
+    @reserv = Reservation.find(params[:id])
+  end
 
 end
