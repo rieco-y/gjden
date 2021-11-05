7f3f8bc71e948dca30cd6931b8679f0ed67208d2
rieco-y <90105763+rieco-y@users.noreply.github.com>
2021-11-05 18:54:25 +0900

    コントローラー不要な記述の削除、@reservationの定義

--- app/controllers/looks_controller.rb
+++ app/controllers/looks_controller.rb
@@ -1,22 +1,15 @@
 class LooksController < ApplicationController
-  #before_action :reservation_params
 
 
   def create
     @look = current_user.looks.create(reservation_id: params[:reservation_id])
-    redirect_back(fallback_location: root_path)
+    @reservation = @look.reservation
   end
 
   def destroy
     @look = Look.find_by(reservation_id: params[:reservation_id], user_id: current_user.id)
+    @reservation = @look.reservation
     @look.destroy
-    redirect_back(fallback_location: root_path)
   end
-
-  #private
-
-  #def reservation_params
-    #@reserv = Reservation.find(params[:id])
-  #end
 
 end
